7ca23d60d85da0bb98e61f8fe817662165154995
Eoin <eoin.yates@gmail.com>
2022-02-01 20:50:21 +0000

    help

--- PythonExample/main.py
+++ PythonExample/main.py
@@ -1,4 +1,26 @@
-a = 5 
-print(a)
-b = 5
-print(b)
+#!/usr/bin/python3
+
+import speedtest
+import logging
+
+class NetworkTest:
+
+    def __init__(self):
+        '''Constructor for class'''
+        self.speed = speedtest.Speedtest()
+
+    def check_speed(self):
+        '''Check speed of both checks'''
+        try:
+            down_speed = self.speed.download() / 1048576
+            up_speed = self.speed.upload() / 1048576
+            down = round(down_speed)
+            up = round(up_speed)
+            logging.info(down)
+            logging.info(up)
+        except speedtest.SpeedtestException as error:
+            logging.error('Error occurred: {}'.format(error))
+       
+if __name__ == "__main__":
+    network_test = NetworkTest()
+    network_test.check_speed()
